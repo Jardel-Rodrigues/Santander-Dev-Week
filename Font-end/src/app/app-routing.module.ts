443e1436d66926759e8d86f710cec5494f44ce81
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2022-06-26 10:57:53 -0300

    Subindo o front-end e Back-end do projeto

---
+++ Font-end/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovimentacaoNewComponent } from './componets/movimentacao-new/movimentacao-new.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+
+
+const routes: Routes = [
+  {path: 'movimentacoes-new', component: MovimentacaoNewComponent},
+  {path: 'movimentacoes', component: MovimentacaoListComponent},
+  { path: 'correntistas', component: CorrentistaComponent },
+  {path: '', redirectTo: 'movintacoes', pathMatch: 'full'},
+];
+ 
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
